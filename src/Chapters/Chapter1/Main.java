548747a2a4eac5e1958f0be28817ef876dc9dafb
KugelbergDK <kugelberg.dk@gmail.com>
2018-09-23 18:55:25 +0200

    Made some assignments in chapter 1

--- src/Chapters/Chapter1/Main.java
+++ src/Chapters/Chapter1/Main.java
@@ -13,16 +13,13 @@ public class Main {
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
 
-        // Give me some space
-        System.out.println("\n\n");
-        // 1.2
-        System.out.println("CHAPTER: 1.2");
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.2");
         String tekst = "Welcome to Java\n";
         System.out.println(tekst+tekst+tekst+tekst+tekst);
 
-        // Give me some space
-        System.out.println("\n\n");
-        System.out.println("CHAPTER: 1.3");
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.3");
         System.out.println(""+
                 "    J     A     V     V    A\n"+
                 "    J    A A     V   V    A A\n"+
@@ -31,9 +28,8 @@ public class Main {
         );
 
 
-        // Give me some space
-        System.out.println("\n\n");
-        System.out.println("CHAPTER: 1.4");
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.4");
 
         int startNumber = 1;
         int limit = 5;
@@ -43,17 +39,14 @@ public class Main {
             System.out.println(x + " \t " + Math.pow(x, 2) + " \t " + Math.pow(x, 3) + " \t " + Math.pow(x, 4));
         }
 
-        // Give me some space
-        System.out.println("\n\n");
-        System.out.println("CHAPTER: 1.5");
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.5");
         System.out.println("Trying to make a calculation");
         System.out.println((9.5 * 4.5 - 2.5 * 3 )/(45.5 - 3.5));
 
 
-        // Give me some space
-        System.out.println("\n\n");
-        System.out.println("CHAPTER: 1.6");
-
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.6");
 
 
         int old = 0;
@@ -62,13 +55,58 @@ public class Main {
 
             if (i == 9){
                 System.out.print(i);
+                System.out.println(" = " +old);
             }
             else {
                 System.out.print(i + "+");
             }
 
         }
-        System.out.println(" = " +old);
+
+
+
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.7");
+        System.out.println("Calculating pi...\nOpgave 1.7");
+        double pi = 4 * (1.0 - (1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+        System.out.println(pi);
+
+
+        System.out.println("\nThe best and most tastefull pi in a for loop");
+        double calculatePi = 0;
+        boolean switcher = true;
+        for (double i = 3; i < 6000000  ; i+=2) {
+            if (switcher){
+                calculatePi += 1/i;
+            }
+            else {
+                calculatePi -= 1/i;
+            }
+            switcher = !switcher;
+        }
+        System.out.println(4 * (1 - calculatePi));
+        System.out.println("\nJava's pi calculation: " + Math.PI);
+
+
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.8");
+        System.out.println("Area and perimeter of circle");
+        double radius = 5.5;
+        double circlePerimeter = 2 * radius * Math.PI;
+        double circleArea = radius * radius * Math.PI;
+        System.out.println("radius: " + radius);
+        System.out.println("perimater: " + circlePerimeter);
+        System.out.println("Area: " + circleArea);
+
+
+        // Give me some space and title
+        System.out.println("\n\nCHAPTER: 1.9");
+        System.out.println("Area and perimeter of a rectangle");
+        double rectangleWidth = 4.5;
+        double rectangleHeight = 7.9;
+        double rectangleArea = rectangleWidth*rectangleHeight;
+        System.out.println("Area of rectangle: " + rectangleArea);
+
 
 
     }
