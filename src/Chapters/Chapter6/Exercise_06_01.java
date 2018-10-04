e47c39e2dd62c93f06e8b283ab21a1911b3c6e72
KugelbergDK <kugelberg.dk@gmail.com>
2018-10-04 13:22:32 +0200

    Exersice_06_01 done

--- src/Chapters/Chapter6/Exercise_06_01.java
+++ src/Chapters/Chapter6/Exercise_06_01.java
@@ -1,7 +1,21 @@
 package Chapters.Chapter6;
 
 public class Exercise_06_01 {
+
+    private static int getPentagonalNumber(int n){
+        return n * (3 * n - 1) / 2;
+    }
+
     public static void main(String[] args) {
-        
+        final int LINE_LIMIT = 10;
+
+        for (int i = 1; i<= 100; i++){
+            if (i % LINE_LIMIT == 0){
+                System.out.println(getPentagonalNumber(i) + " ");
+            }
+            else {
+                System.out.print(getPentagonalNumber(i) + " ");
+            }
+        }
     }
 }
